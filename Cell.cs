669a86586334eac40e3353763871365f71136e4b
Alex <alex.lauer@yahoo.de>
2020-03-02 12:50:32 +0100

    Add project files.

---
+++ Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conveys_Game_of_Life
+{
+    class Cell
+    {
+        public bool Alive { get; set; }
+
+        public Cell(bool alive)
+        {
+            this.Alive = alive;
+        }
+
+
+    }
+}
